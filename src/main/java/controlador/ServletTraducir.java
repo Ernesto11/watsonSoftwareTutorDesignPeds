aeb5a52bbdbcb97cb101283468002cbd6fa3cbc6
Ernesto Brenes C <ernestobrenes96@hotmail.com>
2016-11-04 16:28:14 -0600

    prueba Servlet

--- src/main/java/controlador/ServletTraducir.java
+++ src/main/java/controlador/ServletTraducir.java
@@ -2,14 +2,10 @@ package controlador;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import dto.DTO_Consulta;
-import logicaDeNegocio.Voz;
 
 /**
  * Servlet implementation class ServletTraducir
@@ -25,34 +21,13 @@ public class ServletTraducir extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-    
-    
-    public void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
-    	
-    	String pregunta = request.getParameter("pregunta");
-    	String respuestas =  request.getParameter("respuestas");
-    	
-    	String preguntaIngles = ControladorWeb.solicitudTraducirAIngles(pregunta);
-    	String respuestaIngles = ControladorWeb.solicitudTraducirAIngles(respuestas);
-    	
- 	    DTO_Consulta nuevaConsulta = new DTO_Consulta();
-    	
-    	nuevaConsulta.getRespuestas().add(respuestaIngles);
-    	nuevaConsulta.setPreguntaTexto(preguntaIngles);
-    	
-    	
-    	
-        request.setAttribute("DTO_Consulta", nuevaConsulta);
-        request.getRequestDispatcher("respuestaConsulta.jsp").forward(request, response);
-    	
-    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		processRequest(request, response);
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
@@ -60,7 +35,7 @@ public class ServletTraducir extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		processRequest(request, response);
+		doGet(request, response);
 	}
 
 }
